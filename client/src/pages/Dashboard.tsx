babfa4a60a6dba25dddcae8f5e3f5caf07a98072
Jason Crane <jasonlcrane@gmail.com>
2026-01-31 23:08:18 -0600

    Place forecast widget on dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -86,6 +86,8 @@ export default function Dashboard() {
         </p>
       </div>
 
+      <ForecastWidget />
+
       {/* Statistics Cards */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <Card>
